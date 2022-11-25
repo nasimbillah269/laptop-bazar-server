ad26fe2a998f82523e7b763e08b2d0bd79db8738
nasimbillah <nasimbillah269@gmail.com>
2022-11-25 09:38:55 +0600

    get bookings data from database

--- index.js
+++ index.js
@@ -42,6 +42,13 @@ async function run() {
             res.send(result)
         });
 
+        app.get('/bookings', async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email }
+            const bookings = await bookingsCollection.find(query).toArray();
+            res.send(bookings)
+        })
+
     }
     finally {
 
