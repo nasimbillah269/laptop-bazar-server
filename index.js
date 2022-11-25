1a69672682e26105db33bb8ffb489aec1568cd62
nasimbillah <nasimbillah269@gmail.com>
2022-11-25 14:46:54 +0600

    added my product delete button and product deleted

--- index.js
+++ index.js
@@ -63,6 +63,12 @@ async function run() {
             res.send(addProducts)
         })
 
+        app.delete('/addProducts/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: ObjectId(id) }
+            const result = await addProductsCollection.deleteOne(filter);
+            res.send(result)
+        })
     }
     finally {
 
